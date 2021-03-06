c981202f59f7b69289054225f873a6208cd19158
Thanadol <thanadol.29@mail.kmutt.ac.th>
2021-03-06 17:01:33 +0700

    FINAL push
    update profile to make it work

--- 62130500030_groupwork_6/main.js
+++ 62130500030_groupwork_6/main.js
@@ -24,15 +24,21 @@ const constraints = {
     }
 }
 
+// url:'images/pfp.jpg'
+function submitForm() {
+    alert("Your profile is updated successfully.");
+  }
+
 const app = Vue.createApp({
         data() {
             return {
-            firstname: null,
-            lastname: null,
-            gender: null,
-            email: null,
-            phone: null,
-            url:'images/pfp.jpg'
+                firstname: null,
+                lastname: null,
+                age: null,
+                email: null,
+                phone: null,
+                url:'images/pfp.jpg',
+                errors: null,
             }
         },
         methods: {
@@ -40,8 +46,10 @@ const app = Vue.createApp({
                 this.errors = validate({firstname: this.firstname,
                                         lastname: this.lastname,
                                         age: this.age,
+                                        gender: this.gender,
                                         email: this.email,
-                                        phone: this.phone,},
+                                        phone: this.phone,
+                                        chosensubject: this.chosensubject},
                                         constraints)
                 if(this.errors){
                     e.preventDefault();
